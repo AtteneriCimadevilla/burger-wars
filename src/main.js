759b7e33cdcc08179f3cd3b0a43053a4724a404b
Atteneri Maria Rodriguez Sanchez <attrodsan@outlook.com>
2025-02-21 20:13:45 +0100

    Copied main.js.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,72 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
+import burgers from './assets/data/burgers.json'
+import restaurants from './assets/data/restaurants.json'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import Home from './components/Home.vue'
+import BurgerView from './views/BurgerView.vue'
+import RestaurantsView from './views/RestaurantsView.vue'
+import RestaurantView from './views/RestaurantView.vue'
+import ListOfBurgers from './components/ListOfBurgers.vue'
+import AboutUsView from './views/AboutUsView.vue'
+
+burgers.forEach(burger => {
+    burger.rating = Math.round((burger.ratings.taste + burger.ratings.presentation + burger.ratings.quality_price) / 3 * 100) / 100.00
+});
+
+burgers.sort((a, b) => b.rating - a.rating);
+
+localStorage.setItem('burgers', JSON.stringify(burgers));
+localStorage.setItem('restaurants', JSON.stringify(restaurants));
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/burgers',
+            name: 'burgers',
+            component: ListOfBurgers
+        },
+        {
+            path: '/burger/:id',
+            name: 'burger',
+            component: BurgerView
+        },
+        {
+            path: '/restaurants',
+            name: 'restaurants',
+            component: RestaurantsView
+        },
+        {
+            path: '/restaurant/:id',
+            name: 'restaurant',
+            component: RestaurantView
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutUsView
+        },
+        {
+            path: '/:catchAll(.*)',
+            redirect: '/'
+        },
+        {
+            path: '/list',
+            name: 'list',
+            component: ListOfBurgers
+        }
+    ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
